95281f331c700b4d7ed810750c0e0291924db699
mikal zheng <mikal.zheng@epitech.eu>
2026-04-14 13:13:05 +0200

    feat (task 2.3.1): Fetches the HelloApp custom resource by name and namespace.

--- hello-operator/internal/controller/helloapp_controller.go
+++ hello-operator/internal/controller/helloapp_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
@@ -37,20 +21,14 @@ type HelloAppReconciler struct {
 //+kubebuilder:rbac:groups=apps.intern.dev,resources=helloapps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.intern.dev,resources=helloapps/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the HelloApp object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
 func (r *HelloAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
-
+	var helloApp appsv1alpha1.HelloApp
+	if err := r.Get(ctx, req.NamespacedName, &helloApp); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	logger.Info("Successfully fetched HelloApp", "name", helloApp.Name, "namespace", helloApp.Namespace)
 	return ctrl.Result{}, nil
 }
 
